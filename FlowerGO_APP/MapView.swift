a8e0761f59fb1cc1dba296728215231fc26333af
pgyy <peigen.yuan@tufts.edu>
2024-07-11 13:07:03 -0400

    added the tufts campus to MapView

--- FlowerGO_APP/MapView.swift
+++ FlowerGO_APP/MapView.swift
@@ -4,14 +4,20 @@
 //
 //  Created by Peigen Yuan on 7/10/24.
 //
-
+import MapKit
 import SwiftUI
 
 struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.4075, longitude: -71.1190), span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
+    
     var body: some View {
-        Text("MapView")
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
     }
 }
+
+
 
 #Preview {
     MapView()
